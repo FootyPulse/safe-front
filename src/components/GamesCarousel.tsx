8744ca9ddf67467230d857f3321f66ba6ed640a4
Nick Don <nick.don.1102@gmail.com>
2024-08-15 15:31:00 +1000

    add additional test data

--- src/components/GamesCarousel.tsx
+++ src/components/GamesCarousel.tsx
@@ -107,6 +107,104 @@ const Response = [
       },
     },
   },
+  {
+    game: {
+      id: 2907,
+    },
+    league: {
+      id: 1,
+      season: 2024,
+    },
+    date: "2024-07-25T14:00:00+00:00",
+    time: "14:00",
+    timestamp: "1721916000",
+    timezone: "UTC",
+    round: "Regular Season",
+    week: 20,
+    venue: "Adelaide Oval",
+    attendance: null,
+    status: {
+      long: "Cancelled",
+      short: "CANC",
+    },
+    teams: {
+      home: {
+        id: 1,
+        name: "Adelaide Crows",
+        logo: "https://media.api-sports.io/afl/teams/1.png",
+      },
+      away: {
+        id: 8,
+        name: "Hawthorn Hawks",
+        logo: "https://media.api-sports.io/afl/teams/8.png",
+      },
+    },
+    scores: {
+      home: {
+        score: 0,
+        goals: 0,
+        behinds: 0,
+        psgoals: 0,
+        psbehinds: 0,
+      },
+      away: {
+        score: 0,
+        goals: 0,
+        behinds: 0,
+        psgoals: 0,
+        psbehinds: 0,
+      },
+    },
+  },
+  {
+    game: {
+      id: 2907,
+    },
+    league: {
+      id: 1,
+      season: 2024,
+    },
+    date: "2024-07-25T14:00:00+00:00",
+    time: "14:00",
+    timestamp: "1721916000",
+    timezone: "UTC",
+    round: "Regular Season",
+    week: 20,
+    venue: "Adelaide Oval",
+    attendance: null,
+    status: {
+      long: "Cancelled",
+      short: "CANC",
+    },
+    teams: {
+      home: {
+        id: 1,
+        name: "Adelaide Crows",
+        logo: "https://media.api-sports.io/afl/teams/1.png",
+      },
+      away: {
+        id: 8,
+        name: "Hawthorn Hawks",
+        logo: "https://media.api-sports.io/afl/teams/8.png",
+      },
+    },
+    scores: {
+      home: {
+        score: 0,
+        goals: 0,
+        behinds: 0,
+        psgoals: 0,
+        psbehinds: 0,
+      },
+      away: {
+        score: 0,
+        goals: 0,
+        behinds: 0,
+        psgoals: 0,
+        psbehinds: 0,
+      },
+    },
+  },
 ];
 
 export default function GamesCarousel() {
